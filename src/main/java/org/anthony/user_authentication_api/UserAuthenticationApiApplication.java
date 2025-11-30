18ef67d94c24d80ba62bbebb287ba00a568d7e2d
Anthony <thesuper32203@gmail.com>
2025-11-30 09:13:38 -0500

    first commit

---
+++ src/main/java/org/anthony/user_authentication_api/UserAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package org.anthony.user_authentication_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/anthony/user_authentication_api/UserAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anthony.user_authentication_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
